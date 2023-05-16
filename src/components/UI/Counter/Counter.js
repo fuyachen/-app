5d2f7d936f841200c51c03519bdba86d534f026a
fuyachen <294143413@qq.com>
2023-05-16 23:18:05 +0800

    Counter组件交互

--- src/components/UI/Counter/Counter.js
+++ src/components/UI/Counter/Counter.js
@@ -1,17 +1,28 @@
-import React from "react"
+import React, { useState } from "react"
 import styles from "./Counter.module.css"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faPlus, faMinus } from "@fortawesome/free-solid-svg-icons"
 
-const Counter = () => {
+const Counter = (props) => {
+  const [count, setCount] = useState(0)
+  const sub = () => {
+    setCount((prevCount) => prevCount - 1)
+  }
+  const add = () => {
+    setCount((prevCount) => prevCount + 1)
+  }
   return (
     <div className={styles.container}>
-      <button className={styles.sub}>
-        <FontAwesomeIcon icon={faMinus} />
-      </button>
-      <div className={styles.count}>1</div>
+      {count > 0 ? (
+        <>
+          <button className={styles.sub} onClick={sub}>
+            <FontAwesomeIcon icon={faMinus} />
+          </button>
+          <div className={styles.count}>{count}</div>
+        </>
+      ) : null}
       <button className={styles.add}>
-        <FontAwesomeIcon icon={faPlus} />
+        <FontAwesomeIcon icon={faPlus} onClick={add} />
       </button>
     </div>
   )
